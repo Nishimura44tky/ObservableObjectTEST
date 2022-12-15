53d284406843f28fef52104cc63027c0fcadc6ec
Nishimura44tky <83286066+Nishimura44tky@users.noreply.github.com>
2022-12-15 16:27:37 +0900

    Initial Commit

---
+++ ObservableObjectTEST/ObservableObjectTESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableObjectTESTApp.swift
+//  ObservableObjectTEST
+//
+//  Created by 西村拓也 on 2022/12/15.
+//
+
+import SwiftUI
+
+@main
+struct ObservableObjectTESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
